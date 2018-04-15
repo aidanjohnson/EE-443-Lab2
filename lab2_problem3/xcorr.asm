7ef1ee23ff5f75317c493d40cfcaf7bd07a150c3
Aidan Johnson <34667756+aidanjohnson@users.noreply.github.com>
2018-04-14 18:54:19 -0700

    debugging of .asm in progress; ccs abnormal

--- lab2_problem3/xcorr.asm
+++ lab2_problem3/xcorr.asm
@@ -1,27 +1,39 @@
 			.def _xcorr
 
-_xcorr:			 	    	;cross correlation
-			ZERO A1			;temp storage for each product
-			ZERO B1			;temp storage for sum of products
+_xcorr:			 	    	;auto (cross with self) correlation
 			MV B4,A0		;N samples counter, n
-			MV A6,B0		;K coefficient counter, k
+			MV A6,B0		;K coefficient or shift counter, k
+			MV A4,A3		;resets n+k to n
 
 corr:
+
+shift:
+			ADD A3,4,A3		;shifts by next k
+			SUB B0,1,B0		;decrements shift
+	[B0]	B	shift
+
+			NOP 5
+
+			MV A6,B0		;resets shift counter
 			SUB A0,B0,A0	;shifts N samples by k
-			LDW *A4,B4		;resets n+k to n
-			ADD B4,B0,B4	;shifts by next k
+			ZERO A1			;temp storage for each product
+			ZERO B1			;temp storage for sum of products
+			ZERO A2
+			ZERO B2
 
 sum:						;correlation summation
-			LDW *A4,A2
-			LDW *B4,B2
+			LDW *A4++,A2		;multiplier
+			LDW *A3++,B2		;multiplicand
+			NOP 8
 			MPY A2,B2,A1	;multiplies in1 and in2 at n and n+k sample index
+			NOP 1
 			ADD B1,A1,B1	;sums product
 			SUB A0,1,A0		;decrements samples counter
-
 	[A0]    B sum
-			STW B1,*B6
-			ADD B6,4,B6
+			NOP 5
+
+			STW B1,*B6		;stores correlation coefficient
+			ADD B6,4,B6		;increments to next coefficient store
 			SUB B0,1,B0		;decrements coefficient counter
 			MV B4,A0		;resets samples counter
-
 	[B0]    B	corr
