472e043239f82aa4a099045c912591ac2cf14d89
Deniz Alpay <31429071+dalpay@users.noreply.github.com>
2018-04-15 18:50:12 -0700

    Unverified first go at p4.

--- lab2_problem4/ISRs.c
+++ lab2_problem4/ISRs.c
@@ -9,6 +9,7 @@
 ///////////////////////////////////////////////////////////////////////
 
 #include "DSP_Config.h" 
+#include <stdlib.h>
   
 // Data is received as 2 16-bit words (left/right) packed into one
 // 32-bit word.  The union allows the data to be accessed as a single 
@@ -17,15 +18,121 @@
 
 #define LEFT  0
 #define RIGHT 1
+#define A 0								// Index for channel A
+#define B 1								// Index for channel B
+#define MAX_DELAY 700
+
+int idx = 0;							// Index for buffer
+float channDelay[2] = {(float)MAX_DELAY, (float)MAX_DELAY/2};
+float channGain[2] = {0.0, 1.0};
+int channGainInc[2] = {1, 0};			// 1 if channel gain is increasing, 0 if decreasing
+int upwardPitch = 1;					// 1 if upward pitch change, 0 if downward pitch change
+int pitchShiftIsOn = 1;					// 1 if pitch shifter is enabled, 0 is disabled
+volatile float buffer[MAX_DELAY + 1];	// 0 to 700
+
 
 volatile union {
 	Uint32 UINT;
 	Int16 Channel[2];
 } CodecDataIn, CodecDataOut;
 
+void updateDelays()
+///////////////////////////////////////////////////////////////////////
+// Purpose:   Updates channel A and B delays
+//
+// Input:     None
+//
+// Returns:   Nothing
+//
+// Calls:     None
+//
+// Notes:     None
+///////////////////////////////////////////////////////////////////////
+{
+	int k;
+	// For channel A, k = 0 and for channel B, k = 1
+	for (k = 0; k < 2; k++) {
+		if (upwardPitch) {
+			if (channDelay[A] <= 0) {
+				channDelay[A] = (float)MAX_DELAY;
+			} else {
+				channDelay[A] -= 0.5;
+			}
+		} else {
+			if (channDelay[A] >= 1) {
+				channDelay[A] = (float)MAX_DELAY;
+			} else {
+				channDelay[A] += 0.5;
+			}
+		}
+	}
+}
 
-/* add any global variables here */
+void updateGains()
+///////////////////////////////////////////////////////////////////////
+// Purpose:   Updates channel A and B gains and increase/decrease flags
+//
+// Input:     None
+//
+// Returns:   Nothing
+//
+// Calls:     None
+//
+// Notes:     None
+///////////////////////////////////////////////////////////////////////
+{
+	// For channel A, k = 0 and for channel B, k = 1
+	int k;
+	for (k = 0; k < 2; k++) {
+		if (channGainInc[k]) {
+			if (channGain[k] >= 1) {
+				channGainInc[k] = 0;
+				channGain[k] -= (float)1/MAX_DELAY;
+			} else {
+				channGain[k] += (float)1/MAX_DELAY;
+			}
+		} else {
+			if (channGain[k] <= 0) {
+				channGainInc[k] = 1;
+				channGain[k] += (float)1/MAX_DELAY;
+			} else {
+				channGain[k] -= (float)1/MAX_DELAY;
+			}
+		}
+	}
+}
 
+float* getDelayedVals()
+///////////////////////////////////////////////////////////////////////
+// Purpose:   Gets the delayed values for each channel
+//
+// Input:     None
+//
+// Returns:   Pointer to two floats
+//
+// Calls:     None
+//
+// Notes:     None
+///////////////////////////////////////////////////////////////////////
+{
+	// Allocate memory for two floats
+	float* delayedVal = (float*)malloc(sizeof(float)*2);
+
+	// For channel A, k = 0 and for channel B, k = 1
+	int k;
+	for (k = 0; k < 2; k++) {
+		int ind = idx;
+		int i;
+		// Go back channDelay[k] element to find the delayed value
+		for (i = 0; i < channDelay[k]; i++) {
+			if (--ind <= 0)
+				ind = MAX_DELAY;
+		}
+		delayedVal[k] = buffer[ind];
+	}
+
+	return delayedVal;
+}
 
 interrupt void Codec_ISR()
 ///////////////////////////////////////////////////////////////////////
@@ -40,29 +147,46 @@ interrupt void Codec_ISR()
 // Notes:     None
 ///////////////////////////////////////////////////////////////////////
 {                    
-	/* add any local variables here */
-	float xLeft, xRight, yLeft, yRight;
-
-
  	if(CheckForOverrun())					// overrun error occurred (i.e. halted DSP)
 		return;								// so serial port is reset to recover
 
-  	CodecDataIn.UINT = ReadCodecData();		// get input data samples
-	
-	/* add your code starting here */
+  	CodecDataIn.UINT = ReadCodecData();
 
-	// this example simply copies sample data from in to out
-	xLeft  = CodecDataIn.Channel[ LEFT];
-	xRight = CodecDataIn.Channel[ RIGHT];
+  	// Toggle the pitch shifter and upward/downward pitch change
+  	Int32 SW = ReadSwitches();
 
-	yLeft  = xLeft;
-	yRight = xRight;
+  	if (SW == 0) { 			// SW5 up, SW6 up:
+  		upwardPitch = 1;
+  		pitchShiftIsOn = 1;
+  	} else if (SW == 1) { 	// SW5 down, SW6 up:
+  		upwardPitch = 1;
+  		pitchShiftIsOn = 0;
+  	} else if (SW == 2) {	// SW5 up, SW6 down:
+  		upwardPitch = 0;
+  		pitchShiftIsOn = 1;
+  	} else if (SW == 3) { 	// SW5 down, SW6 down:
+  		upwardPitch = 0;
+  		pitchShiftIsOn = 0;
+  	}
 
-	CodecDataOut.Channel[ LEFT] = yLeft;
-	CodecDataOut.Channel[RIGHT] = yRight;
+  	// Update delays and gains
+  	updateDelays();
+	updateGains();
 
-	/* end your code here */
+  	// Update buffer
+	buffer[idx] = CodecDataIn.Channel[LEFT];
+	if (++idx >= MAX_DELAY)
+		idx = 0;
 
-	WriteCodecData(CodecDataOut.UINT);		// send output data to  port
+	// Prepare output
+	if (pitchShiftIsOn) {
+		float* delayedChann = getDelayedVals();
+		CodecDataOut.Channel[LEFT] = channGain[A]*delayedChann[A] + channGain[B]*delayedChann[B];
+	} else {
+		CodecDataOut.Channel[LEFT] = CodecDataIn.Channel[LEFT];
+	}
+	CodecDataOut.Channel[RIGHT] = CodecDataIn.Channel[RIGHT];
+
+	WriteCodecData(CodecDataOut.UINT);
 }
 
