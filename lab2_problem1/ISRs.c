addded90541fbcc7696455fc99f88b21bf1ca629
Deniz Alpay <31429071+dalpay@users.noreply.github.com>
2018-04-16 18:19:24 -0700

    Updated delays

--- lab2_problem1/ISRs.c
+++ lab2_problem1/ISRs.c
@@ -10,7 +10,7 @@
 
 #include "DSP_Config.h" 
 #include "Echo.h"
-  
+
 // Data is received as 2 16-bit words (left/right) packed into one
 // 32-bit word.  The union allows the data to be accessed as a single 
 // entity when transferring to and from the serial port, but still be 
@@ -24,35 +24,39 @@ volatile union {
 	Int16 Channel[2];
 } CodecDataIn, CodecDataOut;
 
-/* add any global variables here */
 float xLeft, xRight, yLeft, yRight;
-Uint32 oldest = 0; // index for buffer value
-#define DELAY 2
-#define BUFFER_LENGTH DELAY*16000 // buffer length in samples = sampling rate in Hz * delay in s
-#pragma DATA_SECTION (buffer, "CE0"); // put "buffer" in SDRAM
-volatile float buffer[DELAY][BUFFER_LENGTH]; // space for left + right
-int DelaySamples = (int) BUFFER_LENGTH;  // can be manipulated by GEL file
-volatile float gain = 1; /* set gain value for echoed sample */
+int idx = 0; 					// index for buffer value
+#define MAX_DELAY 8000 			// maximum delay
+float buffer[2][MAX_DELAY + 1]; // space for left + right
+int delaySamples = 0; 			// number of samples to delay by
 
-void ZeroBuffer()
+
+float getDelayedVals(int k)
 ///////////////////////////////////////////////////////////////////////
-// Purpose:   Initial fill of all buffer locations with 0.0
+// Purpose:   Gets the delayed values for each channel
 //
 // Input:     None
 //
-// Returns:   Nothing
+// Returns:   Pointer to two floats
 //
-// Calls:     Nothing
+// Calls:     None
 //
 // Notes:     None
 ///////////////////////////////////////////////////////////////////////
 {
-    int i;
+	// Allocate memory for two floats
+	float delayedVal;
 
-    for(i=0; i < BUFFER_LENGTH; i++)  {
-        buffer[LEFT][i] = 0.0;
-        buffer[RIGHT][i] = 0.0;
-        }
+	// For left channel, k = 0 and for right channel, k = 1
+	int ind;	// Index of the delayed value in the buffer
+	if (delaySamples > idx) {
+		ind = MAX_DELAY - (delaySamples - idx);
+	} else {
+		ind = idx - delaySamples;
+	}
+	delayedVal = buffer[k][ind];
+
+	return delayedVal;
 }
 
 interrupt void Codec_ISR()
@@ -68,36 +72,34 @@ interrupt void Codec_ISR()
 // Notes:     None
 ///////////////////////////////////////////////////////////////////////
 {                    
-	/* add any local variables here */
-
  	if(CheckForOverrun())					// overrun error occurred (i.e. halted DSP)
 		return;								// so serial port is reset to recover
 
   	CodecDataIn.UINT = ReadCodecData();		// get input data samples
 	
-	/* add your code starting here */
 	Int32 SW = ReadSwitches();
-
-	if(SW % 2 == 1) // ignore SW5 (= 2^0)
-		SW -= 1;
 
 	// check switches: SW6 = 2^1, SW7 = 2^2; 0 = up, 1 = down
 	// LEDs: D4 = 2^0, D5 = 2^1; 0 = off, 1 = on
 	if(SW == 2) { // SW6 down, SW7 up: right out off, D4 off and D5 on
-		DelaySamples = 600;
-		WriteLEDs(2);
+		delaySamples = 600;
+		WriteLEDs(5);
 	}
 	else if(SW == 4) { // SW6 up, SW7 down: left out off, D4 on and D5 off
-		DelaySamples = 400;
-		WriteLEDs(1);
+		delaySamples = 400;
+		WriteLEDs(3);
 	}
 	else if(SW == 6) { // SW6 and SW7 down: right and left out off, D4 and D5 off
-		DelaySamples = 800;
-		WriteLEDs(0);
+		delaySamples = 800;
+		WriteLEDs(1);
 	}
 	else if(SW == 0) { // SW6 and SW7 up: right and left out on, D4 and D5 on
-		DelaySamples = 200;
-		WriteLEDs(3);
+		delaySamples = 200;
+		WriteLEDs(7);
+	}
+	else  {
+		delaySamples = 0;
+		WriteLEDs(0);
 	}
 
 
@@ -107,21 +109,28 @@ interrupt void Codec_ISR()
 	xLeft = CodecDataIn.Channel[LEFT];   // current LEFT input value to float
 	xRight = CodecDataIn.Channel[RIGHT];   // current RIGHT input value to float
 
-	buffer[LEFT][oldest] = xLeft;
-	buffer[RIGHT][oldest] = xRight;
-	if (++oldest >= DelaySamples) // implement circular buffer
-		oldest = 0;
+	buffer[LEFT][idx] = xLeft;
+	buffer[RIGHT][idx] = xRight;
+	if (++idx >= MAX_DELAY) // implement circular buffer
+		idx = 0;
 
 	// use either FIR or IIR lines below
 
 	// for FIR comb filter effect, uncomment next two lines
-	yLeft = xLeft + (gain * buffer[LEFT][oldest]);
-	yRight = xRight + (gain * buffer[RIGHT][oldest]);
+	float leftDelay = getDelayedVals(LEFT);
+	float rightDelay = getDelayedVals(RIGHT);
+	if (delaySamples == 0) {
+		yLeft = xLeft;
+		yRight = xRight;
+	}
+	else {
+		yLeft = xLeft + leftDelay;
+		yRight = xRight + rightDelay;
+	}
 
 	CodecDataOut.Channel[LEFT] = yLeft;   // setup the LEFT value
 	CodecDataOut.Channel[RIGHT] = yRight; // setup the RIGHT value
 	/*****************************/
-	/* end your code here */
 
 	WriteCodecData(CodecDataOut.UINT);		// send output data to  port
 }
